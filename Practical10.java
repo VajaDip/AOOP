6111d5e6e217fa8f6b1e6762612efebc87c80e4b
VajaDip <162326394+VajaDip@users.noreply.github.com>
2025-04-03 09:29:00 -0700

    Create Practical10.java

---
+++ Practical10.java
@@ -0,0 +1,8 @@
+
+ public class practical10{
+     final int count =2100 ;
+     public static void main(String[] args) {
+         practical10 x1 = new practical10();
+         System.out.println(x1.count);
+     }
+ }
