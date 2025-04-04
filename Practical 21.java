33d705e42879fb4fb9579cee5e467234a82188b2
VajaDip <162326394+VajaDip@users.noreply.github.com>
2025-04-03 23:40:02 -0700

    Create Practical 21.java

---
+++ Practical 21.java
@@ -0,0 +1,11 @@
+final class d1{
+    public void r1(){
+        System.out.println("final class");
+    }
+}
+public class practical21{
+    public static void main(String[] args) {
+        d1 obj=new d1();
+        obj.r1();
+    }
+}
