e9cf496dc6e502e8636fb377bd8a9b0191ed100c
VajaDip <162326394+VajaDip@users.noreply.github.com>
2025-04-03 23:47:32 -0700

    Create Practical 22.java

---
+++ Practical 22.java
@@ -0,0 +1,7 @@
+import mypackage.packageClass;
+public class practical22 {
+    public static void main(String[] args) {
+        packageClass mc = new packageClass();
+        mc.printIntro();
+    }
+}
