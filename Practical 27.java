32a93bf8c5aac3a8adc8d01fba9ead55695de83f
VajaDip <162326394+VajaDip@users.noreply.github.com>
2025-04-04 02:17:12 -0700

    Create Practical 27.java

---
+++ Practical 27.java
@@ -0,0 +1,21 @@
+
+import java.io.*;
+
+public class practical27
+  {
+    public static void practical27(String [] args) throw toException
+  {
+    String filepath="sample.txt";
+    filewriter write=newfilewriter(filepath);
+    write.write("Hello file handling example");
+    write.close();
+    System.out.println("Data write in file");
+    Buffered Reader(new file Reader(filepath));
+    String line;
+    System.out.println("Reading string ");
+    while(line=readerreadline()=null){
+      System.out.println(line);
+    }
+    header clase();
+  }
+  }
